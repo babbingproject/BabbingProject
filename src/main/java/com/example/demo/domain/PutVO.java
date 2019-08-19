3b1778914381ddd51e72246b116382bab40d6d5a
taewon <tonyzorz@naver.com>
2019-08-19 17:35:17 +0900

    5:35

---
+++ src/main/java/com/example/demo/domain/PutVO.java
@@ -0,0 +1,27 @@
+package com.example.demo.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class PutVO {
+
+	@Id @GeneratedValue
+	private int put_id;
+	@Temporal(value= TemporalType.TIMESTAMP)
+	private Date put_time;
+	private int advertisement_id;
+	private int user_id;
+}
